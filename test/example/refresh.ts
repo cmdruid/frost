2ebe10de41b5442d2304719a5cb802e88a33cf12
Christopher Scott <cscottdev@protonmail.com>
2024-11-02 23:06:07 -0500

    fix typos

--- test/example/refresh.ts
+++ test/example/refresh.ts
@@ -37,18 +37,18 @@ const share_1 = get_share(group.shares, 1)
 const share_2 = get_share(group.shares, 2)
 const share_3 = get_share(group.shares, 3)
 
-// Generate recovery shares from each member.
+// Generate refresh shares from each member.
 const ref_shares_1 = gen_refresh_shares(1, thold, share_ct).shares
 const ref_shares_2 = gen_refresh_shares(2, thold, share_ct).shares
 const ref_shares_3 = gen_refresh_shares(3, thold, share_ct).shares
 const ref_shares_g = [ ref_shares_1, ref_shares_2, ref_shares_3 ]
 
-// Distribute and aggregate the recovery shares from each member.
+// Collect the refresh shares from each member.
 const agg_shares_1 = collect_shares(ref_shares_g, 1)
 const agg_shares_2 = collect_shares(ref_shares_g, 2)
 const agg_shares_3 = collect_shares(ref_shares_g, 3)
 
-// Provide the aggregate recovery shares to the target participant.
+// Update the existing share to the new share.
 const new_share_1 = refresh_share(agg_shares_1, share_1)
 const new_share_2 = refresh_share(agg_shares_2, share_2)
 const new_share_3 = refresh_share(agg_shares_3, share_3)
